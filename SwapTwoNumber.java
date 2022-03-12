916ee0868f13d95f340d2022643c571688dea3f3
akshay dhangar <akshaygdhangar14@gmail.com>
2022-03-12 13:03:04 +0530

    Swap number program

---
+++ SwapTwoNumber.java
@@ -0,0 +1,24 @@
+package day5coreJavafunctions;
+
+import java.util.Scanner;
+
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+	int a, b;
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter first Number ");
+	a =sc.nextInt();
+	System.out.print("Enter Second Number ");
+	b =sc.nextInt();
+	sc.close();
+	a=a+b; 
+	b=a-b;
+	a=a-b;
+
+	System.out.println("After Swaping Two numbers ");
+
+	System.out.println("First Number: "+a);
+	System.out.println("Second Number : " +b);
+}
+}
