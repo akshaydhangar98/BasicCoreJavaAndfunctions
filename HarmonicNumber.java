cb61c1a1af5c536a7d41a86146b190f878c22cf6
akshay dhangar <akshaygdhangar14@gmail.com>
2022-03-12 12:54:16 +0530

    Print Harmonic number

---
+++ HarmonicNumber.java
@@ -0,0 +1,24 @@
+package day5coreJavafunctions;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+	public static void main(String[] args) {
+		int N;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Nth value Of Harmonic Number");
+		N = sc.nextInt();
+		sc.close();
+		if (N > 0) {
+			for (int count = 1; count <= N; count++) {
+				System.out.print(1 + "/" + count + " " + "+" + " ");
+			}
+
+		} else {
+
+			System.out.println("Invalid Input ");
+
+		}
+
+	}
+	}
